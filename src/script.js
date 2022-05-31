af779596f4d3dc58ecde4ae7cfd1a049856d4c21
Hun Kim <codinghunkim@gmail.com>
2022-05-31 14:49:04 -0400

    feature('add-scss'): add ability to change how many items per page and add lazy loading images

--- src/script.js
+++ src/script.js
@@ -2,9 +2,10 @@ const auth = "27499358-5a2fddce0b3687817b0f4c648";
 const url = new URL(location.href);
 const params = new URLSearchParams(url.search);
 const currentPage = params.has('page') ? new URL(location.href).searchParams.get('page') : 1;
+const currentItemsPerPage = params.has('perPage') ? new URL(location.href).searchParams.get('perPage') : 10;
 
-const getImages = async (pageNumber) => {
-    const url = `https://pixabay.com/api/?key=${auth}&image_type=photo&per_page=10&q=cute-dog&orientation=vertical&page=${pageNumber}`
+const getImages = async (pageNumber, perPage) => {
+    const url = `https://pixabay.com/api/?key=${auth}&image_type=photo&per_page=${perPage}&q=cute-dog&orientation=vertical&page=${pageNumber}`
     const response = await fetch(
         url,
         {
@@ -54,25 +55,25 @@ const createModal = (image) => {
 
 const renderPagination = (totalHits) => {
     const pagination = document.getElementById("pagination-container");
-    let totalPages = Math.ceil(totalHits / 10);
+    let totalPages = Math.ceil(totalHits / currentItemsPerPage);
 
     const backToFirstPage = document.createElement("a");
     backToFirstPage.innerHTML = "&laquo;";
-    backToFirstPage.href = "/?page=1";
+    backToFirstPage.href = `/?page=1${`&perPage=${currentItemsPerPage}`}`;
 
     const backOnePage = document.createElement("a");
     backOnePage.innerHTML = "&lt;";
     const backOnePageValue = parseInt(currentPage) - 1;
-    backOnePage.href =`/?page=${currentPage == 1 ? 1 : backOnePageValue}`
+    backOnePage.href =`/?page=${currentPage == 1 ? 1 : backOnePageValue}${`&perPage=${currentItemsPerPage}`}`
 
     const forwardOnePage = document.createElement("a");
     forwardOnePage.innerHTML = "&gt;";
     const forwardOnePageValue = parseInt(currentPage) + 1;
-    forwardOnePage.href =`/?page=${currentPage == totalPages ? totalPages : forwardOnePageValue}`
+    forwardOnePage.href =`/?page=${currentPage == totalPages ? totalPages : forwardOnePageValue}${`&perPage=${currentItemsPerPage}`}`
 
     const forwardToLastPage = document.createElement("a");
     forwardToLastPage.innerHTML = "&raquo;";
-    forwardToLastPage.href = `/?page=${totalPages}`;
+    forwardToLastPage.href = `/?page=${totalPages}${`&perPage=${currentItemsPerPage}`}`;
 
     const currentPageNumber = document.createElement("p");
     currentPageNumber.innerText = `${currentPage} of ${totalPages}`;
@@ -84,21 +85,33 @@ const renderPagination = (totalHits) => {
     pagination.appendChild(forwardToLastPage);    
 }
 
-const resetHTML = () => {
-    document.getElementById("image-gallery").innerHTML = "";
+const lazyLoadImages = () => {
+    if ("IntersectionObserver" in window) {
+        const lazyImages = document.querySelectorAll(".lazy");
+        const imageObserver = new IntersectionObserver((entries, observer) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    setTimeout(() => {
+                        const image = entry.target;
+                        image.src = image.dataset.src;
+                        image.classList.remove("lazy");
+                        imageObserver.unobserve(image);
+                    }, 500)
+                   
+                }
+            });
+        });
+        lazyImages.forEach((image) => {
+            imageObserver.observe(image);
+        })
+    }
 }
 
 const renderImages = (images) => {
-    // Remove previous content
-    resetHTML();
-
     images.forEach(image => {
         const newImage = document.createElement("img");
-        newImage.src = image.largeImageURL;
-        newImage.className = "image-gallery__img";
-
-        // Add mango as alt image
-        newImage.setAttribute("alt", image.tags);
+        newImage.setAttribute('data-src', image.largeImageURL);
+        newImage.classList.add("image-gallery__img", "lazy");
         document.getElementById("image-gallery").appendChild(newImage);
 
         const modal = createModal(image);
@@ -108,7 +121,7 @@ const renderImages = (images) => {
         modalCloseBtn.addEventListener("click", () => toggleModel(image.id));
         newImage.addEventListener("click", () => toggleModel(image.id));
     })
-
+    lazyLoadImages();
 }
 
 const renderError = () => {
@@ -119,10 +132,25 @@ const renderError = () => {
     container.appendChild(errorMessage);
 }
 
-getImages(currentPage).then((images) =>{
+const initializePerPage = (perPage) => {
+    const perPageSelect = document.getElementById("per-page__select");
+    perPageSelect.value = perPage;
+    perPageSelect.addEventListener('change', (e) => {
+        const value = e.target.value;
+        url.searchParams.set("perPage", value)
+        window.location = url.href;
+    })
+}
+
+getImages(currentPage, currentItemsPerPage).then((images) =>{
     renderImages(images.hits);
     renderPagination(images.totalHits);
+    initializePerPage(currentItemsPerPage);
 }).catch(error => {
     renderError();
 })
 
+const headerLogo = document.getElementById("logo");
+headerLogo.addEventListener('click', () => {
+    alert("Thank you! Doggo Loves Head Scrutches!! Woof!");
+})
